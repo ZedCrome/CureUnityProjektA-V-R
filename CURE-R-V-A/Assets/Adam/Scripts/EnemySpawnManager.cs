0db8edd35981c2ce25d3310d5b69427eec3364c7
Adam Forsell <adafor2001@skola.goteborg.se>
2020-10-26 16:01:15 +0100

    adam commit

--- CURE-R-V-A/Assets/Adam/Scripts/EnemySpawnManager.cs
+++ CURE-R-V-A/Assets/Adam/Scripts/EnemySpawnManager.cs
@@ -12,19 +12,24 @@ public class EnemySpawnManager : MonoBehaviour
 
     float spawnX1, spawnX2, spawnY1, spawnY2;
 
+    public int = 
+
 
     // Start is called before the first frame update
     void Start()
     {
         spawnX1 = area.transform.position.x - 8f;
         spawnX2 = area.transform.position.x + 8f;
+        spawnY1 = area.transform.position.y - 8f;
+        spawnY2 = area.transform.position.y + 8f;
 
         spawnFrom = Vector2(Random.Range(spawnX1, spawnX2));
+        spawnTo = Vector2(Random.Range(spawnY1, spawnY2));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int randomSpawn = (int)Random.Range(1, )
     }
 }
