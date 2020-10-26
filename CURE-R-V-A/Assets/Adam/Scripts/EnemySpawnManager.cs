4f786f3ee8fa5b9af228634ce29aa76d990685f1
Adam Forsell <adafor2001@skola.goteborg.se>
2020-10-27 00:07:17 +0100

    Adam_Update_02
    -Added prefabs for spawnPoints for enemies(germs) - now theres the main body(center block), head, hands and feet.
    - Enemies now spawn every 3-12 seconds,
    
    - Having issues with making the player collide with trigger of enemy.

--- CURE-R-V-A/Assets/Adam/Scripts/EnemySpawnManager.cs
+++ CURE-R-V-A/Assets/Adam/Scripts/EnemySpawnManager.cs
@@ -4,32 +4,86 @@ using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
 {
-    public GameObject[] enemies = new GameObject[6];
-    public GameObject area = new GameObject();
+    public enemyScript enemyPrefab;
 
-    public Vector2 spawnFrom = new Vector2();
-    public Vector2 spawnTo = new Vector2();
+    public float spawnFrom;
+    public float spawnTo;
 
-    float spawnX1, spawnX2, spawnY1, spawnY2;
+    [SerializeField] float minSpawnTime = 3f;
+    [SerializeField] float maxSpawnTime = 12f;
 
-    public int = 
+    [SerializeField] int maxEnemyNumber = 20, healthPoints = 100, damageLevel = 5;
+
+    float spawnX1, spawnX2, spawnY1, spawnY2, spawnTime, currentTime,
+    damageRate = 3, nextDamage;
+
+    int amountOfEnemies;
+
+    public int randomRoof;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        spawnX1 = area.transform.position.x - 8f;
-        spawnX2 = area.transform.position.x + 8f;
-        spawnY1 = area.transform.position.y - 8f;
-        spawnY2 = area.transform.position.y + 8f;
-
-        spawnFrom = Vector2(Random.Range(spawnX1, spawnX2));
-        spawnTo = Vector2(Random.Range(spawnY1, spawnY2));
+        spawnX1 = transform.localPosition.x - transform.localScale.x/2;
+        spawnX2 = transform.localPosition.x + transform.localScale.x/2;
+        spawnY1 = transform.localPosition.y - transform.localScale.y/2;
+        spawnY2 = transform.localPosition.y + transform.localScale.y/2;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        int randomSpawn = (int)Random.Range(1, )
+        spawnFrom = Random.Range(spawnX1, spawnX2);
+        spawnTo = Random.Range(spawnY1, spawnY2);
+    }
+
+
+    void FixedUpdate()
+    {
+        enemyScript[] enemies = FindObjectsOfType<enemyScript>();
+
+        for (int i = 0; i < enemies.Length; i++)
+        {
+            if (enemies[i] == null)
+            {
+                continue;
+            }
+        }
+
+        Vector2 randomSpawnPosition = new Vector2(spawnFrom, spawnTo);
+
+        if (currentTime + spawnTime < Time.time && amountOfEnemies < maxEnemyNumber )
+        {
+            Instantiate(enemyPrefab, randomSpawnPosition, Quaternion.identity);
+
+            currentTime = Time.time;
+            SetSpawnTime();
+
+            amountOfEnemies ++;
+        }
+    }
+
+
+    void SetSpawnTime()
+    {
+        spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+    }
+
+
+    void CauseDamage()
+    {
+        if(amountOfEnemies > 0 && amountOfEnemies < damageLevel && Time.time > nextDamage)
+        {
+            nextDamage = Time.time + damageRate;
+            healthPoints --;
+
+            if(amountOfEnemies >= damageLevel)
+            {
+                damageLevel += 5;
+                damageRate -= 0.1f; 
+            }
+        }
     }
 }
---
+++ CURE-R-V-A/Assets/enemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    public int healthPoints = 20;
+
+    public float damageRate = 0.2f, nextDamage;
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            if(Time.time > nextDamage)
+            {
+                healthPoints --;
+
+                nextDamage = Time.time + damageRate;
+
+            }
+        }
+
+        Debug.Log("Touching Player");
+    }
+
+    void Update()
+    {
+        if(healthPoints < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
