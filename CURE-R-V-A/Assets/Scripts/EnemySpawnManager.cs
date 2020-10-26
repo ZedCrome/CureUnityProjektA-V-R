459bf3822ff1a4532fd577cbbcf62d56d945d177
Adam Forsell <adafor2001@skola.goteborg.se>
2020-10-26 14:38:38 +0100

    Adam Commit_01

---
+++ CURE-R-V-A/Assets/Scripts/EnemySpawnManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = new GameObject[5];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
