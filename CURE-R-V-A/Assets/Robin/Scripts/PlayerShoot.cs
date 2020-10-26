0422125c9fbb85105ba07a113fd4d03cc8394d92
ZedCrome <robin.lindevy@live.com>
2020-10-26 15:39:43 +0100

    Improved structure to make working individually easier

---
+++ CURE-R-V-A/Assets/Robin/Scripts/PlayerShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public Transform gun;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 20f;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = bulletPrefab.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject newBullet = Instantiate(bulletPrefab, gun.position, gun.rotation);
+
+            //rb2d.AddForce(gun.up * bulletSpeed, ForceMode2D.Impulse);
+            rb2d.velocity = gun.transform.up * bulletSpeed;
+
+        }
+    }
+}
