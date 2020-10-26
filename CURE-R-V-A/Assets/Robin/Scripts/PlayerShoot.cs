b64429c33bd93c982bdba9874ca34f1c0782475d
ZedCrome <robin.lindevy@live.com>
2020-10-26 20:41:56 +0100

    particleShitFak:(

--- CURE-R-V-A/Assets/Robin/Scripts/PlayerShoot.cs
+++ CURE-R-V-A/Assets/Robin/Scripts/PlayerShoot.cs
@@ -1,29 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem ps;
+
+    [SerializeField] private ParticleSystem.EmissionModule emission;
+
+    [SerializeField] private float emissionRate;
 
     public Transform gun;
     public GameObject bulletPrefab;
     public float bulletSpeed = 20f;
+    public float reload = 0f;
+    public float coolDownPeriod = 1f;
     private Rigidbody2D rb2d;
 
     void Start()
     {
         rb2d = bulletPrefab.GetComponent<Rigidbody2D>();
+        ps = GetComponent<ParticleSystem>();
+        emission = ps.emission;
     }
 
-    void Update()
+    void LateUpdate()
     {
-        if (Input.GetButtonDown("Fire1"))
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButton("Fire1") && reload <= Time.time)
         {
-            GameObject newBullet = Instantiate(bulletPrefab, gun.position, gun.rotation);
 
-            //rb2d.AddForce(gun.up * bulletSpeed, ForceMode2D.Impulse);
-            rb2d.velocity = gun.transform.up * bulletSpeed;
+            GameObject newBullet = Instantiate(bulletPrefab, gun.transform.position, gun.transform.rotation);
+            newBullet.GetComponent<Rigidbody2D>().velocity = gun.transform.up * bulletSpeed;
 
+            reload = Time.time + coolDownPeriod;
+
+            emissionRate = emission.rateOverTime.constant;
+            emissionRate--;
+            emission.rateOverTime = emissionRate;
+            Debug.Log(emissionRate);
         }
     }
 }
---
+++ CURE-R-V-A/Assets/Robin/Scripts/SmoothCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCamera : MonoBehaviour
+{
+
+    public Transform player;
+
+    public int zOffset = -10;
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = smoothedPosition;
+    }
+}
