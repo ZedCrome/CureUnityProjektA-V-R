ed871b76ee962c01d26864d681e398630f7eef5b
ZedCrome <robin.lindevy@live.com>
2020-10-28 17:45:02 +0100

    Work IN Progress mAp

---
+++ CURE-R-V-A/Assets/Scripts/mapScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mapScore : MonoBehaviour
+{
+    public int bodyScore;
+    [SerializeField] private GameObject bodyPart;
+
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bodyScore = bodyPart.GetComponent<EnemySpawnManager>().enemyCount;
+        scoreText.text = bodyScore.ToString();
+    }
+}
